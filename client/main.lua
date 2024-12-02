95fb42d329519b01269c6f1349cf11410146de5e
talmidel <anthony.charpenay@proton.me>
2024-12-02 17:47:51 +0100

    Add driving school marker & refactor config.lua

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,54 @@
+local drivingSchoolPos = _G.Config.DrivingSchool.Coordinates
+
 -- Create driving school blip
 CreateThread(function ()
-    local pos = _G.Config.Coordinates.DrivingSchool.Position
-    local blip = AddBlipForCoord(pos.x, pos.y, pos.z)
+    local blip = AddBlipForCoord(drivingSchoolPos.x, drivingSchoolPos.y, drivingSchoolPos.z)
+    local drivingSchoolBlip = _G.Config.DrivingSchool.Blip
 
-    SetBlipSprite(blip, _G.Config.Blip.Sprite)
-    SetBlipColour(blip, _G.Config.Blip.Color)
-    SetBlipDisplay(blip, _G.Config.Blip.Display)
+    SetBlipSprite(blip, drivingSchoolBlip.Sprite)
+    SetBlipColour(blip, drivingSchoolBlip.Color)
+    SetBlipDisplay(blip, drivingSchoolBlip.Display)
 
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentString("Auto École")
     EndTextCommandSetBlipName(blip)
+end)
+
+-- Create driving school origin marker
+CreateThread(function ()
+    while true do
+        local playerCoords = GetEntityCoords(PlayerPedId()) -- get current player coordinates
+        local distanceFromSchool = #(playerCoords - vector3(drivingSchoolPos.x, drivingSchoolPos.y, drivingSchoolPos.z)) -- get distance from marker
+
+        if distanceFromSchool < _G.Config.DrawDistance then
+            local drivingSchoolMarker =  _G.Config.DrivingSchool.Marker
+
+            DrawMarker(
+                drivingSchoolMarker.Type,
+                drivingSchoolPos.x,
+                drivingSchoolPos.y,
+                drivingSchoolPos.z,
+                drivingSchoolMarker.Direction.x,
+                drivingSchoolMarker.Direction.y,
+                drivingSchoolMarker.Direction.z,
+                drivingSchoolMarker.Rotation.x,
+                drivingSchoolMarker.Rotation.y,
+                drivingSchoolMarker.Rotation.z,
+                drivingSchoolMarker.Scale.x,
+                drivingSchoolMarker.Scale.y,
+                drivingSchoolMarker.Scale.z,
+                drivingSchoolMarker.Color.r,
+                drivingSchoolMarker.Color.g,
+                drivingSchoolMarker.Color.b,
+                drivingSchoolMarker.Color.a,
+                drivingSchoolMarker.Bobbing,
+                drivingSchoolMarker.FaceCamera,
+                drivingSchoolMarker.P19,
+                drivingSchoolMarker.Rotation.rotate,
+                drivingSchoolMarker.Texture.dict,
+                drivingSchoolMarker.Texture.name,
+                drivingSchoolMarker.DrawOnEnts
+            )
+        end
+    end
 end)
